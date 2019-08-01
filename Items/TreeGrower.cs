b1bb21498ff758c70edf04c251102c53dbcc6ed5
Instrex <instrex2@gmail.com>
2019-08-01 14:47:05 +0300

    separated files

---
+++ Items/TreeGrower.cs
@@ -0,0 +1,21 @@
+﻿using Terraria.ModLoader;
+
+namespace MechanismsMod.Items {
+    public class TreeGrower : ModItem
+    {
+        public override string Texture => "MechanismsMod/Items/Mech";
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = 1;
+            item.consumable = true;
+            item.createTile = mod.TileType("TreeGrowerTile");
+        }
+    }
+}
---
+++ Tiles/TreeChopperTile.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace MechanismsMod.Items {
+    public class TreeChopperTile : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+            Main.tileLighted[Type] = true;
+            dustType = -1;
+            drop = mod.ItemType("TreeChopper");
+            AddMapEntry(Color.Gray);
+        }
+
+        Vector2 rect = new Vector2(8, 4);
+        Vector2 origin = new Vector2(12, 12);
+        Vector2 heights = new Vector2(0, -16);
+
+        public Texture2D item
+        {
+            get { return Main.itemTexture[ItemID.CopperAxe]; }
+        }
+
+        public override bool PreDraw(int i, int j, SpriteBatch spriteBatch)
+        {
+            DrawItem(i, j, spriteBatch);
+            if (Main.tile[i, j].inActive())
+            {
+                Active(i, j);
+            }
+
+            for (int side = -1; side <= 1; side += 2)
+            {
+                for (int y = (j - (int)rect.Y) * 16; y <= (j + (int)rect.Y + 1) * 16; y++)
+                {
+                    var d = Dust.NewDustPerfect(new Vector2((i + (side == 1 ? 1 : 0) + rect.X * side) * 16, y), DustID.WitherLightning);
+                    d.velocity = Vector2.Zero;
+                    d.noGravity = true;
+                    d.scale = 0.3f;
+                }
+            }
+
+            return base.PreDraw(i, j, spriteBatch);
+        }
+
+        public void DrawItem(int i, int j, SpriteBatch spriteBatch)
+        {
+            Vector2 pos = (new Vector2(i, j) + origin) * 16 + heights - Main.screenPosition;
+            spriteBatch.Draw(item, new Rectangle((int)pos.X, (int)pos.Y, 16, 16), Color.White * Main.essScale);
+        }
+
+        int[] tilesToDestroy = { TileID.Trees };
+        public override void HitWire(int i, int j)
+        {
+            Main.tile[i, j].inActive(!Main.tile[i, j].inActive());
+        }
+
+        public void Active(int i, int j)
+        {
+            for (int x = i - (int)rect.X; x <= i + (int)rect.X; x++)
+            {
+                for (int y = j - (int)rect.Y; y <= j + (int)rect.Y; y++)
+                {
+
+                    if (WorldGen.InWorld(x, y))
+                    {
+                        var t = Main.tile[x, y].type;
+                        var up = Main.tile[x, y - 1].type;
+                        var up2 = Main.tile[x, y - 2].type;
+                        var under = Main.tile[x, y + 1].type;
+
+                        if (tilesToDestroy.Contains(t) && under == TileID.Grass)
+                        {
+                            TileHelper.DamageTile(x, y, 5);
+                            if (up == TileID.Trees && up2 == TileID.Trees)
+                            {
+                                WorldGen.PlaceTile(x, y, 20);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
