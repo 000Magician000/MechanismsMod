0f15d8b644899d6aaa7861f7313ed2d886f56895
Magician/// <123.thunder@mail.ru>
2019-08-04 19:10:44 +0500

    RMB fix

--- Items/Tools/Wrench.cs
+++ Items/Tools/Wrench.cs
@@ -5,14 +5,18 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using System.Linq;
 
-namespace MechanismsMod.Items.Tools {
-    public class Wrench : ModItem {
-        public override void SetStaticDefaults() {
+namespace MechanismsMod.Items.Tools
+{
+    public class Wrench : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
             Tooltip.SetDefault("Hit machine to configure/scan");
             Tooltip.SetDefault("Right click machine for alternate configuration");
         }
 
-        public override void SetDefaults() {
+        public override void SetDefaults()
+        {
             item.UseSound = SoundID.Item1;
             item.useAnimation = 20;
             item.useTime = 20;
@@ -23,27 +27,27 @@ namespace MechanismsMod.Items.Tools {
             item.autoReuse = true;
         }
 
-        public override bool AltFunctionUse(Player player) {
-            if (player.altFunctionUse == 2) {
-                if (TileEntity.ByPosition.TryGetValue(new Point16((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16),
-                out var tileEntity) && tileEntity is IWrenchConfigurable mechanic) {
-
-                    mechanic.AltConfigure(player);
-                    Main.PlaySound(0, player.Center, 0);
-                }
-            }
-
+        public override bool AltFunctionUse(Player player)
+        {
             return true;
         }
 
-        public override bool UseItem(Player player) {
+        public override bool UseItem(Player player)
+        {
             if (TileEntity.ByPosition.TryGetValue(new Point16((int)Main.MouseWorld.X / 16, (int)Main.MouseWorld.Y / 16),
-                out var tileEntity) && tileEntity is IWrenchConfigurable mechanic) {
+                out var tileEntity) && tileEntity is IWrenchConfigurable mechanic)
+            {
+                if (player.altFunctionUse == 2)
+                {
+                    mechanic.AltConfigure(player);
+                }
+                else
+                {
+                    mechanic.Configure(player);
+                }
 
-                mechanic.Configure(player);
-                Main.PlaySound(0, player.Center, 0);
             }
-
+            Main.PlaySound(0, player.Center, 0);
             return true;
         }
     }
